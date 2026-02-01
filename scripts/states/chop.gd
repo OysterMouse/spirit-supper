197d17e973d9aca0c97a2cb8d7e4a589d15cc0b0
alexosterhoudt <alex_osterhoudt94@yahoo.com>
2026-02-01 01:13:57 -0500

    Cleaned up

--- scripts/states/chop.gd
+++ scripts/states/chop.gd
@@ -1,7 +1,11 @@
 extends State
 
+@export var move_state: State
+var is_finished: bool = false
+
 func enter() -> void:
 	super()
+	is_finished = false
 	print("Chopping")
 	parent.velocity = Vector2.ZERO
 
@@ -9,4 +13,11 @@ func process_input(event: InputEvent) -> State:
 	return null
 
 func process_physics(delta: float) -> State:
+	if is_finished:
+		return move_state
 	return null
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if "chop" in anim_name:
+		is_finished = true
+		
