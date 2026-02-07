a1445b6e83ccb59a4401d8b7d73ce062d255b3df
Alex Osterhoudt <alex_osterhoudt94@yahoo.com>
2026-02-06 23:42:52 -0500

    Added animations for goblin archer enemy

--- scripts/state.gd
+++ scripts/state.gd
@@ -2,7 +2,6 @@ class_name State
 extends Node
 
 @export var animation_name : String
-@export var use_blend_space: bool = false
 
 var parent : CharacterBody2D
 var playback : AnimationNodeStateMachinePlayback
@@ -17,9 +16,6 @@ func enter() -> void:
 		if sprite is AnimatedSprite2D and sprite.animation != "":
 			sprite.play()
 	
-	if use_blend_space and parent.has_method("set_blend_position"):
-		update_blend_direction()
-
 func exit() -> void:
 	pass
 
@@ -31,9 +27,3 @@ func process_frame(delta: float) -> State:
 
 func process_physics(delta: float) -> State:
 	return null
-
-func update_blend_direction() -> void:
-	if use_blend_space and parent.has_method("set_blend_position"):
-		var direction = parent.velocity.normalized()
-		if direction.length() > 0.1:
-			parent.set_blend_position(animation_name, direction)
